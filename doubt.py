d81e8175c1ebd9c70021da5d229bf851076dc79a
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2024-11-30 17:21:03 +0300

    feat: upload and dark mode implemented.

--- doubt.py
+++ doubt.py
@@ -1,8 +1,24 @@
-from dash import Dash, html
+import dash
+from dash import Dash, html, dcc, Input, Output, State
+import dash_uploader as du
+import nibabel as nib
+import numpy as np
+import plotly.express as px
 
-app = Dash()
 
-app.layout = [html.Div(children='Hello World')]
+app = dash.Dash(__name__)
+app.title = "Interactive MRI Uncertainty Visualizer"  # Set the browser tab title
+
+du.configure_upload(app, '/tmp/uploads')  # Set upload folder
+
+app.layout = html.Div([
+    html.H1("Interactive MRI Uncertainty Visualizer", style={'textAlign': 'center'}),
+    html.P("Upload, analyze, and visualize uncertainty in brain MRI images.", 
+           style={'textAlign': 'center', 'fontSize': '18px'}), du.Upload( id="upload-data", text="Drag and Drop or Click to Upload", max_files=1, filetypes=['.gz'],),
+    html.Div(id='output-image-upload')])
+
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)
